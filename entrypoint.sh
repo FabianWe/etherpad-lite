46136f8c7d534e4522419c4aff5153d617be9ee9
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2017-04-02 03:38:30 +0200

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# MIT License
+# Copyright (c) 2017 Fabian Wenzelmann
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# NOTE: This file was largely inspired by tvelocity/etherpad-lite:
+# https://github.com/tvelocity/dockerfiles/blob/master/etherpad-lite/entrypoint.sh
+
+set -e
+
+: ${ETHERPAD_DB_HOST:=mysql}
+: ${ETHERPAD_DB_USER:=root}
+: ${ETHERPAD_DB_NAME:=etherpad}
+ETHERPAD_DB_NAME=$( echo $ETHERPAD_DB_NAME | sed 's/\./_/g' )
+
+if [ -z "$ETHERPAD_DB_PASSWORD" ]; then
+	echo >&2 'error: missing required ETHERPAD_DB_PASSWORD environment variable'
+	echo >&2 '  Did you forget to -e ETHERPAD_DB_PASSWORD=... ?'
+	echo >&2
+	echo >&2 '  (Also of interest might be ETHERPAD_DB_USER and ETHERPAD_DB_NAME.)'
+	exit 1
+fi
+
+: ${ETHERPAD_TITLE:=Etherpad}
+: ${ETHERPAD_PORT:=9001}
+
+# Check if database already exists
+RESULT=`mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} \
+	-h${ETHERPAD_DB_HOST} --skip-column-names \
+	-e "SHOW DATABASES LIKE '${ETHERPAD_DB_NAME}'"`
+
+if [ "$RESULT" != $ETHERPAD_DB_NAME ]; then
+	# mysql database does not exist, create it
+	echo "Creating database ${ETHERPAD_DB_NAME}"
+
+	mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} -h${ETHERPAD_DB_HOST} \
+	      -e "create database ${ETHERPAD_DB_NAME}"
+fi
+
+if [ ! -f settings.json ]; then
+
+	cat <<- EOF > settings.json
+	{
+	  "title": "${ETHERPAD_TITLE}",
+	  "ip": "0.0.0.0",
+	  "port" :${ETHERPAD_PORT},
+	  "dbType" : "mysql",
+	  "dbSettings" : {
+			    "user"    : "${ETHERPAD_DB_USER}",
+			    "host"    : "${ETHERPAD_DB_HOST}",
+			    "password": "${ETHERPAD_DB_PASSWORD}",
+			    "database": "${ETHERPAD_DB_NAME}"
+			  },
+	EOF
+
+	if [ $ETHERPAD_ADMIN_PASSWORD ]; then
+
+		: ${ETHERPAD_ADMIN_USER:=admin}
+
+		cat <<- EOF >> settings.json
+		  "users": {
+		    "${ETHERPAD_ADMIN_USER}": {
+		      "password": "${ETHERPAD_ADMIN_PASSWORD}",
+		      "is_admin": true
+		    }
+		  },
+		EOF
+	fi
+
+	cat <<- EOF >> settings.json
+	}
+	EOF
+fi
+
+if [ ! -f APIKEY.txt ]; then
+	API_KEY=$(node -p "require('crypto').randomBytes(32).toString('hex')")
+	echo "${API_KEY}" > APIKEY.txt
+	printf "%s\n" "${API_KEY}"
+fi
+
+if [ ! -f SESSIONKEY.txt ]; then
+	SESSION_KEY=$(node -p "require('crypto').randomBytes(32).toString('hex')")
+	echo "${SESSION_KEY}" > SESSIONKEY.txt
+	printf "%s\n" "${SESSION_KEY}"
+fi
+
+exec "$@"
